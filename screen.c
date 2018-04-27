024c0758b72e1e890df1bcba197d073083517e3b
chuongtran1303 <37363712+chuongtran1303@users.noreply.github.com>
2018-04-27 04:08:26 +0300

    Update screen.c

--- screen.c
+++ screen.c
@@ -4,18 +4,18 @@
 #include "screen.h"
 #include <stdio.h>
 #include "comm.h"
-void clearScreen(void){
+void clearScreen(void){ // erase the total terminal screen with VT100 escape sequence "/ESC[2J"
 	printf("%c[2J", ESC);
 	fflush(stdout);
 }
-void gotoxy(int row, int col){
+void gotoxy(int row, int col){ // set the cursor to the particular location of terminal screen
 	printf("%c[%d;%dH", ESC, row, col);
 	fflush(stdout);
 }
-void setColor(int color){
-	printf("%c[1;%dm", ESC, color);
+void setColor(int color){ // change the terminal text color
+	printf("%c[1;%dm", ESC, color); 
 	fflush(stdout);}
-void bar(int col, double dB){
+void bar(int col, double dB){ // draw the bar by the value of dB 
 	int i;
 	for(i=0; i<dB/4; i++){
 	   gotoxy(25-i, col+1); //the 1st bar start from col=1
