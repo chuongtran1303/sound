8b78e4722cb1f90d6d2d25ee3d331f760877573a
chuongtran1303 <37363712+chuongtran1303@users.noreply.github.com>
2018-04-30 17:03:45 +0300

    Update comm.h

--- comm.h
+++ comm.h
@@ -1,4 +1,4 @@
 /* This file contain the sever address in order to let the project know the "direction" */
 #define COMM // compilation conditional
 #define URL "http://www.cc.puv.fi/~e1700692/sound.php"
-void sendToServer(double []);
+void sendToServer(double []); //Function prototype
